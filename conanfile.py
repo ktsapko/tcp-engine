e53a0dfcd6672e564c02b82c81142d8d6f8548f3
Kyrylo <ktsapko@gmail.com>
2026-07-19 16:41:39 +0300

    Add Conan 2 support

---
+++ conanfile.py
@@ -0,0 +1,32 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+
+
+class TcpEngineConan(ConanFile):
+    name = "tcp-engine"
+    version = "0.1.0"
+    package_type = "application"
+
+    settings = "os", "compiler", "build_type", "arch"
+
+    generators = "CMakeDeps"
+
+    exports_sources = (
+        "CMakeLists.txt",
+        "app/*",
+        "src/*",
+        "include/*",
+        "tests/*",
+    )
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        toolchain = CMakeToolchain(self)
+        toolchain.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
